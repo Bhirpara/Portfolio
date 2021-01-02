565a22d25e55944954e37046297a911fad74c405
bhirpara <bhirpara@lakeheadu.ca>
2021-01-02 00:14:58 -0500

    initial commit completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import MyNavbar from "./components/my-navbar/my-navbar.component";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
+import MyTitleMessage from "./components/title-message/title-message.component";
 
 const App = () => {
   return (
     <div className="App">
+    <MyCarousal />
+    <MyTitleMessage />
     <MyNavbar />
-    My Portfolio
+    
     </div>
   );
 }
