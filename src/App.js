2c7964377bf47782b02889d8a3d62a184ddbacef
bhirpara <bhirpara@lakeheadu.ca>
2021-01-01 22:45:15 -0500

    initial commit completed navbar component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MyNavbar from "./components/my-navbar/my-navbar.component";
+
+const App = () => {
+  return (
+    <div className="App">
+    <MyNavbar />
+    My Portfolio
+    </div>
+  );
+}
+
+export default App;
